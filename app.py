c4913ca352a8e5e25f4d5233c8d1e9121d157203
Ankitr324 <ankitr080@gmail.com>
2026-06-21 09:13:30 +0530

    track the model with git LFS

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle
+from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model=load_model('nextword_model1.h5')
+
+with open('tokenizer.pkl', 'rb') as file:
+    tokenizer=pickle.load(file)
+
+reverse_index={idx:word for word, idx in tokenizer.word_index.items()}
+
+max_len=170
+
+st.title("Next Word Prediction with Deep Learning")
+
+def generate_text(seed_text, num_words=10):
+    text=seed_text
+    for _ in range (num_words):
+        seq=tokenizer.texts_to_sequences([text])[0]
+        padded=pad_sequences([seq], maxlen=max_len, padding='pre')
+        preds=model.predict(padded, verbose=0)
+        pos=np.argmax(preds)
+        next_word=reverse_index.get(pos, " ")
+        text+=" "+next_word
+    return text
+
+
+seed=st.text_input("Enter a starting text :", "Hello")
+
+num_words=st.slider("Number of words to generate",1,20,10)
+
+if st.button('Generate'):
+    result=generate_text(seed, num_words)
+    st.write(result)
